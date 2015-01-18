95e2bf5c77df68b26a96b36cfe218b655360c111
Matt Gray <matt.gray@7digital.com>
2015-01-18 19:41:50 +0000

    add pkg-config to apt-get build dependencies

--- provisioning/packages.sh
+++ provisioning/packages.sh
@@ -32,7 +32,7 @@ sed -i "s/http.us.debian.org/$MIRROR/g" /etc/apt/sources.list
 apt-get update
 apt-get -y -t wheezy-backports install "git"
 
-apt-get install -y build-essential m4                        # build tools
+apt-get install -y build-essential m4 pkg-config             # build tools
 apt-get install -y xen-tools                                 # xen client tools
 apt-get install -y ocaml ocaml-native-compilers camlp4-extra # ocaml
 apt-get install -y aspcud                                    # opam solver
